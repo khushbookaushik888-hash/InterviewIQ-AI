e889071dd6757522929f499dfcd415bbe63ae9eb
Khushboo Kaushik <khushbookaushik888@gmail.com>
2026-08-31 00:31:07 +0530

    Fix frontend pages and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-
-        <Route path="/" element={<Home />} />
-
-        <Route path="/login" element={<Login />} />
-
-        <Route path="/register" element={<Register />} />
-
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Navbar />
+      <main>
+        <Hero />
+        <Features />
+      </main>
+      <Footer />
+    </>
   );
 }
 
